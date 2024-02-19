80462dbc754a11ac343758b4a91cd6fb17dd2d8a
J <152270265+mavenxj@users.noreply.github.com>
2024-02-19 17:30:30 +0530

    Update chat-ai.py

--- chat-ai.py
+++ chat-ai.py
@@ -1,14 +1,12 @@
 import google.generativeai as genai
 import streamlit as st
+import re
 import os
 import io
 import PIL.Image
 import urllib.request as urllib
 from pathlib import Path
 from gtts import gTTS
-import time
-import keyboard
-import psutil
 
 def main():
     # Customize the layout of chatbot
@@ -18,66 +16,78 @@ def main():
     GOOGLE_API_KEY = st.sidebar.text_input("Google API Key", type="password", key="K1")
     os.environ['GOOGLE_API_KEY'] = GOOGLE_API_KEY
 
-    if not os.getenv('GOOGLE_API_KEY', '').startswith("AI"):
-        st.warning("Note: Enter your Google API key for this chat. https://ai.google.dev/pricing")
+    col1, col2 = st.columns(2, gap="medium")
 
-    # Initialize the GenAI
-    genai.configure(api_key=GOOGLE_API_KEY)
+    with col1:
+        st.header("Query")
+        if not os.getenv('GOOGLE_API_KEY', '').startswith("AI"):
+            st.warning("Note: Enter your Google API key in the sidebar.")
+        
+        # Initialize the GenAI
+        genai.configure(api_key=GOOGLE_API_KEY)
 
-    img = ""    
-    image_url = st.sidebar.text_input("Enter image url:", key="K-img")
+        question = st.text_input("Prompt", key="K-text")
 
-    question = st.text_input("How can the AI assistant assist you today...")
-                             
-    #read image file
-    if image_url:
-        fd = urllib.urlopen(image_url)
-        image_file = io.BytesIO(fd.read())
-        img = PIL.Image.open(image_file)
-        st.image(image_url, width=500)
+        img = ""    
+        image_url = st.text_input("Image URL", key="K-img")
 
-    # Query through AI
-    if img:
-        get_response_image(question, img)
-    elif not question == "":
-        get_response_text(question)
+        #read image file
+        if image_url:
+            fd = urllib.urlopen(image_url)
+            image_file = io.BytesIO(fd.read())
+            img = PIL.Image.open(image_file)
+            st.image(image_url)
+
+    with col2:
+        st.header("Response")
+        
+        # text generation
+        if img:
+            out1 = get_response_image(question, img)
+            # speech conversion
+            TTS(out1)
+            st.write(out1) 
+        elif not question == "":
+            out2 = get_response_text(question)
+            # speech conversion
+            TTS(out2)
+            st.write(out2)
 
 def get_response_image(query, image):
     model = genai.GenerativeModel('gemini-pro-vision')
     response = model.generate_content([query, image], stream=True)
     response.resolve()
-    out = response.text
-    st.write(out)
-    TTS(out)
+    res_img = response.text
+    return res_img
 
-#generative AI for text input
 def get_response_text(query):
     model = genai.GenerativeModel('gemini-pro')
-    chat = model.start_chat()
+    chat = model.start_chat(history=[])
     response = chat.send_message(query, stream=True)
     response.resolve()
-    out = response.text
-    st.write(out)
-    TTS(out)
+    res_text = response.text
+    return res_text
 
-# text to speech
-def TTS(response_ai):
-    # Convert TTS button
-    if st.button("Convert to Speech"):
-        tts = gTTS(text = response_ai, lang='en')
-        filename = 'response.wav'
-        tts.save(filename)
+#text to speech
+def TTS(res):
+    # remove special characters before speech conversion
+    spl_char = '[^a-zA-Z0-9 \n\.]'
+    clean_text = re.sub(pattern=spl_char, repl="", string=res)
 
-        _dir = Path(__file__).parent
-        audio_path = _dir / filename
-        os.environ['AUDIO_KEY'] = str(audio_path)
+    tts = gTTS(text=clean_text, lang='en')
+    filename = 'response.wav'
+    tts.save(filename)
 
-        try:
-            audio_file = open(os.environ['AUDIO_KEY'], 'rb')
-            audio_bytes = audio_file.read()
-            st.audio(audio_bytes, format='audio/wav')
-        except(FileNotFoundError) :
-            st.error("File not found")
+    _dir = Path(__file__).parent
+    audio_path = _dir / filename
+    os.environ['AUDIO_KEY'] = str(audio_path)
+        
+    try:
+        audio_file = open(os.environ['AUDIO_KEY'], 'rb')
+        audio_bytes = audio_file.read()
+        st.audio(audio_bytes, format='audio/wav')
+    except(FileNotFoundError) :
+        st.error("File not found")
 
 if __name__ == "__main__":
   main()
