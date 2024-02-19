483c9f6a6690fbe174f32ad9172c813d9bda8d1b
J <152270265+mavenxj@users.noreply.github.com>
2024-02-19 20:05:29 +0530

    Update chat-ai.py

--- chat-ai.py
+++ chat-ai.py
@@ -29,12 +29,10 @@ def main():
         # Initialize the GenAI
         genai.configure(api_key=GOOGLE_API_KEY)
         
-        st.write("Enter your query")
-        question = st.text_input("",key="K-text")
+        question = st.text_input("Enter your query",key="K-text")
 
         img = ""
-        st.write("Enter image URL")
-        image_url = st.text_input("",key="K-img")
+        image_url = st.text_input("Enter image URL",key="K-img")
 
         #read image file
         if image_url:
